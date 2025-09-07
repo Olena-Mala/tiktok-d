cbac8564a70c50cefed27a91e750ec6fbd477f2d
Olena-Mala <helenvm2903@gmail.com>
2025-09-07 19:22:08 +0200

    app.py

--- app.py
+++ app.py
@@ -78,3 +78,20 @@ def download_video():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/howto')
+def howto():
+    return render_template('howto.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
